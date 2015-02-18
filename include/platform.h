a768c4bc7403b698dc28b885e7289aae4bedbaf0
Mike Davis <mike@lookhere.co.za>
2015-02-18 17:59:43 +0200

    Fix for Linux

--- include/platform.h
+++ include/platform.h
@@ -1,6 +1,8 @@
 #ifndef COMMON_H
 #define COMMON_H
 
+#include <unistd.h>
+
 // With help from https://stackoverflow.com/questions/5919996/how-to-detect-reliably-mac-os-x-ios-linux-windows-in-c-preprocessor
 #ifdef __linux
         #include <netinet/ether.h>
@@ -8,7 +10,6 @@
 #elif __MACH__
 	#include <net/ethernet.h>
 	#include <sys/errno.h>
-	#include <unistd.h>
 	#include <mach/clock.h>
 	#include <mach/mach.h>
 #else
